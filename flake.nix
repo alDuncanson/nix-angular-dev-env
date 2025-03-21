4c9128fd50bd11818364bb3fae4ee5eb5eca4b2d
Al Duncanson <alDuncanson@proton.me>
2025-03-21 19:11:32 -0400

    Add angular development environment flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Angular Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            pkgs.nodePackages."@angular/cli"
+          ];
+
+          shellHook = ''
+            echo "Angular development environment activated 🚀"
+            ng version
+          '';
+        };
+      }
+    );
+}
